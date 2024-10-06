39bc565c27a8f5b60bd537d56c87dad8c09ba34b
rinishat <142586403+rinishat@users.noreply.github.com>
2024-10-06 22:54:44 +0530

    Update Playlists.jsx

--- client/src/pages/Playlists.jsx
+++ client/src/pages/Playlists.jsx
@@ -15,7 +15,7 @@ const Search = () => {
           placeholder="What do you want to listen to?"
           className="w-full py-3 pl-12 pr-4 bg-white/10 rounded-full text-white placeholder-gray-400 focus:outline-none focus:bg-white/20"
         />
-      </div>
+      </div><br></br>
 
       <h2 className="text-2xl font-bold text-white mb-4">Browse All</h2>
       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-4">
